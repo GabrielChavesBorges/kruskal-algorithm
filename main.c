506c696d4440d11ab497422295a1b05afbbec22c
Gabriel Borges <gabriel.chaves.borges@gmail.com>
2023-10-24 17:30:34 +0900

    add example input file

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct edge {
+    int start_node;
+    int end_node;
+    int weight;
+};
+
+int main() {
+    struct edge* edges;
+    int edge_count = 0;
+    
+    // create structs representing edges
+    // apply kruskal's algorithm
+    return 0;
+}
